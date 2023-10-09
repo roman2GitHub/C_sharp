5be85ffd193402c5fef7a532603b189c9483d3c6
RomanSH <roman_2679@mail.ru>
2023-10-09 09:04:38 +0300

    Большее и меньшее из двух чисел

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine(number1 + " большее " + number2 + " меньшее");
+}
+else
+{
+    Console.WriteLine(number1 + " меньшее, " + number2 + " большее");
+}
