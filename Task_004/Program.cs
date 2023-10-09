dd721ed1c07e9378e91e34fde0901062f8e6b8ff
RomanSH <roman_2679@mail.ru>
2023-10-09 13:35:47 +0300

    Решение Task_004

---
+++ Task_004/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число больше единицы: ");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i < number; i = i + 2)
+{
+    Console.Write(i + " ");
+}
+// for (int i = 2; i < number; i+=2)
+// {
+//     Console.Write(i);
+// }
+
