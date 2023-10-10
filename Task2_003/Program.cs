f5ca45a66f507ec55e0e43fcfd889593e7c860ce
RomanSH <roman_2679@mail.ru>
2023-10-10 12:18:48 +0300

    Задача3_15

---
+++ Task2_003/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+while (day < 1 || day > 7)
+{
+    Console.WriteLine("Неверный день недели");
+    return;
+}
+if (day == 6 || day == 7)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
