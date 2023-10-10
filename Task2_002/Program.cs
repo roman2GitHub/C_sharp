26b348077dd7144489d94bcf716c0425fe839392
RomanSH <roman_2679@mail.ru>
2023-10-10 14:39:59 +0300

    Решенные задания ко 2 семинару

---
+++ Task2_002/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100)
+{
+    Console.WriteLine("Введенное число не имеет третьей цифры");
+    return;
+}
+else
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    int ThreeNumber = number % 10;
+
+    Console.WriteLine($"Третья цифра введенного числа равна {ThreeNumber}");
--- Task2_003/Program.cs
+++ Task2_003/Program.cs
@@ -1,15 +1,16 @@
 ﻿Console.WriteLine("Введите номер дня недели: ");
 int day = Convert.ToInt32(Console.ReadLine());
-while (day < 1 || day > 7)
+if (day < 1 || day > 7)
 {
     Console.WriteLine("Неверный день недели");
     return;
 }
-if (day == 6 || day == 7)
-{
-    Console.WriteLine("Да");
-}
 else
-{
-    Console.WriteLine("Нет");
-}
+    if (day == 6 || day == 7)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
