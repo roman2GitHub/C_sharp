d84a1294bd9d4badee28142dbb6088ba76b4ca5e
RomanSH <roman_2679@mail.ru>
2023-10-10 11:00:19 +0300

    задача 10

---
+++ Task2_001/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number < 99 || Number > 999)
+{
+    Console.WriteLine("Введено не трехзначное число");
+}
+else
+{
+    int secondNumber = (Number / 10) % 10;
+    Console.WriteLine($"Вторая цифра трехзначного числа равна: {secondNumber}");
+}
